444eaac4b1b29896fc7e6307099d0109b011f814
shubhushan49 <shubhushan49@outlook.com>
2021-12-25 17:33:17 +0100

    created a different file to handle the heavy lifting and make main.py cleaner

---
+++ helper_functions.py
@@ -0,0 +1,26 @@
+def validate_json():
+    pass
+
+def user_delete():
+    pass
+
+def user_get():
+    pass
+
+def user_put():
+    pass
+
+def user_post():
+    pass
+
+def group_delete():
+    pass
+
+def group_get():
+    pass
+
+def group_post():
+    pass
+
+def group_put():
+    pass
